11a180811b75bc3869ae33457257bc1b937efca0
Karunakaran001 <114465657+Karunakaran001@users.noreply.github.com>
2022-09-27 12:20:40 +0530

    Test 1

--- sum and pass.py.c
+++ sum and pass.py.c
@@ -1,12 +1,3 @@
-#!/usr/bin/python3
-
-import time
- 
-t = time.localtime(time.time())
-localtime = time.asctime(t)
-str = "Current Time:" + time.asctime(t)
- 
-print(str);
 n1 = int(input ('enter num 1:'))
 n2 = int(input ('enter num 2:'))
 n3 = int(input ('enter num 3:'))
